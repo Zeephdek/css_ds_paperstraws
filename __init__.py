cfbe664fa13d9da8964edd222380cc544677d76f
Zeephdek <59129423+Zeephdek@users.noreply.github.com>
2021-10-13 10:54:29 +0800

    more code from today

--- __init__.py
+++ __init__.py
@@ -1,9 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt 
+from matplotlib.ticker import StrMethodFormatter
 import pandas as pd
 
 import utils
 import re
+import json
+
+MRTStations = {
+    "Ang Mo Kio": (1.370017, 103.84945),
+    "Marymount": (1.349167, 103.839444),
+    "Bishan": (1.351111, 103.848333)
+}
 
 def main1():
     list1 = pd.read_csv("list part 1 missing values.csv")
@@ -26,6 +34,105 @@ def seperateCoords(df):
         return ltt, lng, acc
 
     df["lat"], df["long"], df["acc"] = zip(*df["location"].map(parseCoord))
+    return df
+
+def dealingWithCatVars():
+    """
+    This imports the json and seems how to deal with the cat vars.
+
+    """
+    threshold = 0.8
+    catnumber = 5
+
+    with open("variable_occurences_list2.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    """
+    Modifying the cat vars - some errors in data input
+    For establishmentType
+    Hawker -> Hawker Centre
+    Vending machine, Vending -> Vending Machine
+    """
+
+    newEst = {}
+    for a in list(data["establishmentType"]):
+        if a == "Hawker":
+            try: 
+                newEst["Hawker Centre"] += data["establishmentType"][a]
+            except:
+                newEst["Hawker Centre"] = data["establishmentType"][a]
+        elif a == "Vending machine" or a == "Vending":
+            try:
+                newEst["Vending Machine"] += data["establishmentType"][a]
+            except:
+                newEst["Vending Machine"] = data["establishmentType"][a]
+        else:
+            try:
+                newEst[a] += data["establishmentType"][a]
+            except:
+                newEst[a] = data["establishmentType"][a]
+
+
+    data["establishmentType"] = newEst
+    # print(utils.json_beautify(data))
+
+    for col in list(data):
+        vars = dict(sorted(data[col].items(), key=lambda x:x[1]))  # dict - sorted
+        total = sum(vars.values())
+        # print("TOTAL: {}".format(total))
+
+        Ctotal = 0
+        varsToCare = []
+        varsToIgnore = []
+        for var in list(vars):
+            Ctotal += vars[var]
+            if ( total - Ctotal ) / total < threshold:
+                varsToCare.append(var)
+            else:
+                varsToIgnore.append(var)
+
+        print(varsToIgnore, varsToCare)
+
+def distToMrt(df):
+    return ()
+
+def catVar(df):
+
+    ## dealing with the rogue variables first
+    # for EstType
+    df.loc[df['establishmentType'] == "Hawker", 'establishmentType'] = "Hawker Centre"
+    df.loc[df['establishmentType'].isin(["Vending machine", "Vending"]), 'establishmentType'] = "Vending Machine"
+
+    ## then this
+    drinkTypeCats = {
+        "[BOTTLE] Coca-cola/Pepsi Original": 6,
+        "[BOTTLE] Fanta Grape/Orange": 8,
+        "[BOTTLE] Sprite Original": 8,
+        "[CAN] Coca-cola/Pepsi Original": 7,
+        "[CAN] Fanta Grape/Orange": 1,
+        "[CAN] Sprite Original": 3,
+        "[HOME-MADE] Bandung": 8,
+        "[HOME-MADE] Lime Juice": 2,
+        "[HOME-MADE] Milo Original": 4
+    } # alternatively bottles vs can vs homemade - wait lest not deal with this yet
+    estTypeCats = {
+        "Coffee shop": 5,
+        "Convenience stall": 4,
+        "Hawker Centre": 3,
+        "Mall": 2,
+        "Minimart": 6,
+        "Store": 6,
+        "Supermarket": 1,
+        "Vending Machine": 6
+    } # 6 becomes "other"
+
+    for cat in list(estTypeCats):
+        val = estTypeCats[cat]
+
+        df.loc[df['establishmentType'] == cat, "estTypeInt"] = int(val)
+
+    ## yay !
+
     return df
 
 def main2():
@@ -58,22 +165,23 @@ def main2():
     # list2.drop(columns=["Unnamed: 0", "Unnamed: 0.1", "Unnamed: 0.1.1"]) # weird - solution is above, from SO
 
     ## Types of cat vars for drinkType and establishment
-    occurences = {}
+    # Exports to a Json
+    # occurences = {}
 
-    for col in ["drinkType", "establishmentType"]:
-        occurences[col] = {}
+    # for col in ["drinkType", "establishmentType"]:
+    #     occurences[col] = {}
 
-        print("\nColumn: {}".format(col))
-        alreadyNotedVars = []
-        for a in list2[col]:
-            if a not in alreadyNotedVars:
-                alreadyNotedVars.append(a)
-                n = list2[col].value_counts()[a]
-                print("{} - {} occurences".format(a, n))
-                occurences[col][a] = int(n)
+    #     print("\nColumn: {}".format(col))
+    #     alreadyNotedVars = []
+    #     for a in list2[col]:
+    #         if a not in alreadyNotedVars:
+    #             alreadyNotedVars.append(a)
+    #             n = list2[col].value_counts()[a]
+    #             print("{} - {} occurences".format(a, n))
+    #             occurences[col][a] = int(n)
     
-    with open("variable_occurences_list2.json", "w", encoding="utf-8") as f:
-        f.write(utils.json_beautify(occurences))
+    # with open("variable_occurences_list2.json", "w", encoding="utf-8") as f:
+    #     f.write(utils.json_beautify(occurences))
     catvar_occurences = """
         Column: drinkType
         [HOME-MADE] Milo Original - 13 occurences
@@ -104,13 +212,56 @@ def main2():
     # list2 = seperateCoords(list2)
     # list2 = list2.drop(columns=["location"])
 
-    viewDf()
+    ## Dealing with the categorical variables
+    list2 = catVar(list2)
+
+    # viewDf()
 
     ## output to new csv
     # list2.to_csv("list part 2 (MOD).csv", index=False)
 
     ### testing som matplotlib
     # testPlotLocations(list2)
+    
+    plots(list2)
+
+def plots(df):
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+    ax1 = testPlotLocations(df, ax1)
+    ax2 = plotEstType(df, ax2)
+
+    plt.show()
+
+def plotEstType(df, ax):
+    ## DONE
+
+    labels = ['Supermarket', 'Mall', 'Hawker Centre', 'Convenience stall', 'Coffee shop', 'Other'] # 6 
+    means = []
+    std = []
+    maxs = []
+    mins = []
+    for n in range(1,7):
+        means.append(df.loc[df["estTypeInt"] == n]["price"].mean())
+        maxs.append(df.loc[df["estTypeInt"] == n]["price"].max())
+        mins.append(df.loc[df["estTypeInt"] == n]["price"].min())
+
+    x = np.arange(len(labels))
+    width = 0.2
+
+    ax.set_title("Price Based on Type of Establishment") 
+    ax.set_ylabel("Price/$") 
+
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+
+    bars = ax.bar(labels, means, width, yerr=[
+        np.array(means) - np.array(mins), 
+        np.array(maxs) - np.array(means)],
+        label='Establishments')
+
+    ax.bar_label(bars)
+
+    return ax
 
 def testPlot(df):
     """
@@ -125,14 +276,22 @@ def testPlot(df):
     plt.scatter(x, y, color ="red") 
     plt.show()
 
-def testPlotLocations(df):
+def testPlotLocations(df, ax):
     """
     Might be useful to overlay a map as the bg image
     """
     x, y = np.array(df['long']), np.array(df['lat'])
-    plt.title("Coordinates")
-    plt.scatter(x, y)
-    plt.show()
+    NSLy, NSLx = [MRTStations["Bishan"][0], MRTStations["Ang Mo Kio"][0]], [MRTStations["Bishan"][1], MRTStations["Ang Mo Kio"][1]]
+    CCLy, CCLx = [MRTStations["Marymount"][0]], [MRTStations["Marymount"][1]]
+    ax.set_title("Coordinates")
+    ax.scatter(x, y)
+    ax.scatter(CCLx, CCLy, color = "yellow")
+    ax.scatter(NSLx, NSLy, color = "red")
+
+    return ax
+
+def plotDistToMrts(df, ax):
+    return ax
 
 if __name__ == "__main__":
     main2()
